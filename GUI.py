5507448542cbeddc311a51a8e74887c3e343858a
Jiaming927 <masterjm@cs.washington.edu>
2014-05-24 01:42:09 -0700

    Lynx done
    Super Smooooth movement :)

--- GUI.py
+++ GUI.py
@@ -1,5 +1,6 @@
 from Tkinter import *
 from subprocess import call
+import time
 
 class App:
 	def __init__(self, master):
@@ -7,31 +8,55 @@ class App:
 		Label(master, text="Speed:     ").grid(row=1, column = 1)
 		Label(master, text="Position:  ").grid(row=2, column = 1)
 
-		self.servoVal = StringVar()
-		self.servoVal.set("#1")
+		self.smoothSpeed = StringVar()
+		self.smoothPosition = StringVar()
 
-		self.servo = OptionMenu(master, self.servoVal, "#1", "#2", "#3", "#4", "#5", "#6")
+		self.servoVal = StringVar()
+		self.servoVal.set("pick one")
+
+		self.servo = OptionMenu(master, self.servoVal, "pick one", "#0", "#1", "#2", "#3")
 		self.servo.grid(row=0, column=2, padx=5, pady=2.5)
 
+		self.speedEntry = Entry(master, width = 4, textvariable=self.smoothSpeed)
 		self.speed = Scale(master, from_=0, to=100, orient=HORIZONTAL)
 		self.speed.grid(row=1, column=2, padx=5, pady=2.5)
+		self.speedEntry.grid(row=1, column=3, padx=5, pady=1)
 
+		self.positionEntry = Entry(master, width = 4, textvariable=self.smoothPosition)
 		self.position = Scale(master, from_=0, to=100, orient=HORIZONTAL, command=self.position_change)
 		self.position.grid(row=2, column=2, padx=5, pady=2.5)
+		self.positionEntry.grid(row=2, column=3, padx=5, pady=1)
 
 		button = Button(master, text="QUIT", fg="red", command=master.quit)
 		button.grid(row=3, column=1, padx=10, pady=2.5)
 
+		ModeButton = Button(master, text="Move Smoothly", fg="blue", command=self.smooth_position_change)
+		ModeButton.grid(row=3, column=3, padx=10, pady=2.5)
+
 	def position_change(self, position):
-		str = self.servoVal.get()
-		index = str[1]
-		speed = int(self.speed.get()) * 20
+		temp = self.servoVal.get()
+		index = temp[1]
+		speed = int(self.speed.get()) * 100
 		pos = int(position) * 20 + 500
+		print index, pos, speed
 		# print index
 		# print speed
 		# print pos
 		# send index position speed
-		call(["rosrun", "arm_controller", "arm_controller_client", "index", "speed", "pos"])
+		call(["rosrun", "arm_controller", "arm_controller_client", index, str(pos), str(speed)])
+
+	def smooth_position_change(self):
+		print "Smooth Mode"
+		temp = self.servoVal.get()
+		index = temp[1]
+		speed = int(self.smoothSpeed.get()) * 100
+		pos = int(self.smoothPosition.get()) * 20 + 500
+		print index, pos, speed
+		# print index
+		# print speed
+		# print pos
+		# send index position speed
+		call(["rosrun", "arm_controller", "arm_controller_client", index, str(pos), str(speed)])
 
 root = Tk()
 root.title("Arm Controller")
--- arm_controller_server.cpp
+++ arm_controller_server.cpp
@@ -8,27 +8,51 @@
 #include <fcntl.h>
 #include <string>
 #include <sstream>
+#include <unistd.h>
+#include <cmath>
+
 int fd;
+int currPosition[4];
+int current; // Current part
+int idx; // # of servo that's gonna move
+int speed; // Speed
+int rate; // How big is an interval
+int interval; // How many parts there are 
+bool postiveMovement; // Whether it's moving toward larger number or not
 
 bool send(arm_controller::Move::Request  &req,
          arm_controller::Move::Response  &res)
 {
   res.response = "Received";
-  ROS_INFO("sending command: [Index: %ld, Position:%ld, Speed: %ld]", req.index, req.position, req.speed);
+  ROS_INFO("sending command: [idx: %ld, Position:%ld, Speed: %ld]", req.index, req.position, req.speed);
   
-  std::stringstream input;
-  input << "#" << req.index << " P" << req.position << " S" << req.speed;
-  // Turn string into commands
+  current = 0;
+  idx = req.index;
+  speed = req.speed;
+  interval = abs(req.position - currPosition[idx]);
+  int start = currPosition[idx];
 
-  const char *cInput = input.str().c_str();
-  int length = strlen(cInput);
-  char *buff = strndup(cInput, length); 
-  buff[length - 1] = '\r';
-  int written = write(fd, buff, length);
-  free(buff);
+  if (req.position - start > 0) {
+    postiveMovement = true;
+  } else {
+    postiveMovement = false;
+  }
+      
+  currPosition[idx] = req.position;
   return true;
+  // std::stringstream input;
+  // input << "#" << idx << " P" << position << " S" << speed;
+  // // Turn string into commands
+
+  // const char *cInput = input.str().c_str();
+  // int length = strlen(cInput);
+  // char *buff = strndup(cInput, length); 
+  // buff[length - 1] = '\r';
+  // int written = write(fd, buff, length);
+  // free(buff);
 }
 
+// Setup the initizl behavior
 void setup() {
   // get parameter, change ttyUSB0 at runtime
   fd = open("/dev/ttyUSB0", O_RDWR);
@@ -48,7 +72,42 @@ void setup() {
   if (tcsetattr(fd, TCSANOW, &options) != 0) {
     ROS_INFO("apply setting error");
   }
+
+  // MEH
+  // arm_controller::Move::Request req;
+  // req.position = 800;
+  // req.speed = 2000;
+
+  // arm_controller::Move::Response res;
+  // res.response = "";
+
+  // Sets the initial value
+  int setupSpeed = 5000;
+  int setupPosition = 1500;
+  int thirdPosition = 800;
+  for (int i = 0; i < 4; i++) {
+    if (i == 3) {
+      currPosition[i] = thirdPosition;
+      setupPosition = thirdPosition;
+    } else {
+      currPosition[i] = setupPosition;
+      std::stringstream input;
+      input << "#" << i << " P" << setupPosition << " S" << setupSpeed;
+
+      // Writes to the file
+      const char *cInput = input.str().c_str();
+      int length = strlen(cInput);
+      char *buff = strndup(cInput, length); 
+      buff[length - 1] = '\r';
+      int written = write(fd, buff, length);
+      free(buff);
+
+      // Sleep
+      usleep(200);
+    }
+  }
 }
+
 
 int main(int argc, char **argv)
 {
@@ -57,7 +116,34 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
   ros::ServiceServer service = n.advertiseService("arm_controller", send);
   ROS_INFO("Ready to perform actions");
-  ros::spin();
+
+  // Non-blocking solution
+  while (1) {
+      if (current != interval) {
+        ROS_INFO("NUM: %d, idx: %d, SPEED: %d",currPosition[idx], idx, speed);
+
+        current++;
+        if (postiveMovement) {
+          currPosition[idx] += rate;
+        } else {
+          currPosition[idx] -= rate;
+        }
+
+        std::stringstream input;
+        input << "#" << idx << " P" << currPosition[idx] << " S" << speed;
+        
+        // Writes to the file
+        const char *cInput = input.str().c_str();
+        int length = strlen(cInput);
+        char *buff = strndup(cInput, length); 
+        buff[length - 1] = '\r';
+        int written = write(fd, buff, length);
+        free(buff);
+
+        usleep(200);
+      }
+    ros::spinOnce();
+  }
 
   return 0;
 }
