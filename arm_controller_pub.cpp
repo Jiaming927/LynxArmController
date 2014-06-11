197d987b82ba4dc865567db96655c5382b88bf01
Jiaming927 <masterjm@cs.washington.edu>
2014-06-10 21:04:21 -0700

    Some updates

--- arm_controller_pub.cpp
+++ arm_controller_pub.cpp
@@ -2,29 +2,30 @@
 #include "std_msgs/String.h"
 #include <sstream>
 
+
 int main(int argc, char **argv) {
   ros::init(argc, argv, "arm_controller_pub");
   ros::NodeHandle n;
 
   ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000, 1);
+
+  // Ros might not be able to respond that fast,
+  // So we need some way to work around like this while loop
   ros::Rate poll_rate(100);
   while (chatter_pub.getNumSubscribers() == 0) {
     ROS_INFO("Waiting");
     poll_rate.sleep();
   }
 
-  // ros::Rate loop_rate(10);
-  // while (ros::ok()) { 
-    std_msgs::String msg;
-    std::stringstream ss;
-    ss << argv[1] << " " << argv[2] << " " << argv[3];
-    msg.data = ss.str();
-	
-    ROS_INFO("Published %s", msg.data.c_str());
-    chatter_pub.publish(msg);
-    ros::spinOnce();
- // }
-    return 0;
+  // Still takes the data
+  std_msgs::String msg;
+  std::stringstream ss;
+  ss << argv[1] << " " << argv[2] << " " << argv[3];
+  msg.data = ss.str();
+
+  ROS_INFO("Published %s", msg.data.c_str());
+  chatter_pub.publish(msg);
+  return 0;
 }
 
 
